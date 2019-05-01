4f7e6b10c0d9db46dbd5ad8f3a09c10770e849e3
Orestis Tsioukis <orestistsioukis@gmail.com>
2019-05-02 00:03:35 +0300

    adding a div box in icons

--- index.php
+++ index.php
@@ -24,35 +24,43 @@ require "header.php";
 			</div>
 		</div>
 		<div class="icon-container">
-			<div class="icon-rectangular">
-				<h3>Difficulty level</h3>
-				<span>High
-					<div class="sp-visible">Steep terrain that requires moderate level climbing skills on
-						snow/ice/rock.
-						Emphasis on good cramponing skills. Will need experience with rope techniques including
-						rappelling/abseiling and snow camping techniques.
-					</div>
-				</span>
+			<div class="icon-rectangular-big">
+				<div class="icon-box">
+					<h3>Difficulty level</h3>
+					<span>High
+						<div class="sp-visible">Steep terrain that requires moderate level climbing skills on
+							snow/ice/rock.
+							Emphasis on good cramponing skills. Will need experience with rope techniques including
+							rappelling/abseiling and snow camping techniques.
+						</div>
+					</span>
+				</div>
 			</div>
-			<div class="icon-rectangular">
-				<h3>Duration</h3>
-				<p>60-70 days</p>
+			<div class="icon-rectangular-tiny">
+				<div class="icon-box">
+					<h3>Duration</h3>
+					<p>60-70 days</p>
+				</div>
 			</div>
-			<div class="icon-rectangular">
-				<h3>Fitness level</h3>
-				<span>Very high
-					<div class="sp-visible">Excellent level of fitness from participants who would have an ongoing
-						commitment to training and maintaining fitness specific to climbing. Expect long days in
-						extreme
-						conditions. Preparation would include heavy pack
-						carrying, specific conditioning through rock and/or ice climbing and habitual cardiovascular
-						exercise.
-					</div>
-				</span>
+			<div class="icon-rectangular-big">
+				<div class="icon-box">
+					<h3>Fitness level</h3>
+					<span>Very high
+						<div class="sp-visible">Excellent level of fitness from participants who would have an ongoing
+							commitment to training and maintaining fitness specific to climbing. Expect long days in
+							extreme
+							conditions. Preparation would include heavy pack
+							carrying, specific conditioning through rock and/or ice climbing and habitual cardiovascular
+							exercise.
+						</div>
+					</span>
+				</div>
 			</div>
-			<div class="icon-rectangular">
-				<h3>Elevation</h3>
-				<p>8850M/29,035FT</p>
+			<div class="icon-rectangular-tiny">
+				<div class="icon-box">
+					<h3>Elevation</h3>
+					<p>8850M/29,035FT</p>
+				</div>
 			</div>
 		</div>
 		<section class="main-text">
@@ -62,16 +70,17 @@ require "header.php";
 				excitement. With many experienced and professional mountaineering guides to suit your character and
 				needs. And a wide variety of climbing equipment, high altitude clothing and footwear to rent for your
 				trip in Nepal.</article>
+			<div class="paragraph-box">
+				<p>Mount Everest is still the ultimate mountaineering adventure. To stand at the pinnacle of the earth is
+					one of life's most rewarding experiences. As pioneers of guided ascents on Everest, Adventure
+					Consultants is recognised as the premier guiding service with a superb reputation for enabling members
+					of our expeditions to achieve summit aspirations.</p>
 
-			<p>Mount Everest is still the ultimate mountaineering adventure. To stand at the pinnacle of the earth is
-				one of life's most rewarding experiences. As pioneers of guided ascents on Everest, Adventure
-				Consultants is recognised as the premier guiding service with a superb reputation for enabling members
-				of our expeditions to achieve summit aspirations.</p>
-
-			<p>An attempt on Everest is a committing undertaking which requires a huge amount of dedication and
-				determination. If you are serious about achieving the top and feel you have the right ingredients and
-				experience, we invite you to apply for a position on our team in 2019, on what will be our twenty-sixth
-				Everest expedition.</p>
+				<p>An attempt on Everest is a committing undertaking which requires a huge amount of dedication and
+					determination. If you are serious about achieving the top and feel you have the right ingredients and
+					experience, we invite you to apply for a position on our team in 2019, on what will be our twenty-sixth
+					Everest expedition.</p>
+			</div>
 		</section>
 		<div class="wrapper">
 			<div class="video-wrapper">
